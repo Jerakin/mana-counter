a4b22e3ffa77263457a4959a5b4e30a22fe6e266
Jerakin <the_rensvind@hotmail.com>
2022-11-21 23:16:57 +0100

    inital commit

---
+++ main/app/constants.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.White = {}
+M.White.color = vmath.vector3(252/255, 253/255, 180/255)
+M.White.texture = "w"
+
+M.Blue = {}
+M.Blue.color =      vmath.vector3(87/255, 179/255, 242/255)
+M.Blue.texture = "u"
+
+M.Black = {}
+M.Black.color =     vmath.vector3(113/255, 113/255, 113/255)
+M.Black.texture = "b"
+
+M.Red = {}
+M.Red.color =       vmath.vector3(243/255, 60/255, 67/255)
+M.Red.texture = "r"
+
+M.Green = {}
+M.Green.color =     vmath.vector3(37/255, 170/255, 86/255)
+M.Green.texture = "g"
+
+M.Colorless = {}
+M.Colorless.color = vmath.vector3(227/255, 227/255, 227/255)
+M.Colorless.texture = "c"
+
+return M
