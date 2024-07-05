c1e82adcbc1914c1ac903830110bb55189035d75
Jerakin <the_rensvind@hotmail.com>
2024-07-05 08:40:56 +0200

    fix: history number is now on the far right

--- main/app/total_view/history.lua
+++ main/app/total_view/history.lua
@@ -1,13 +1,12 @@
-M = {}
+local M = {}
 
 M.history = {}
-
+M.index = {}
 function M.add(name, number)
 	if number ~= 0 then
 		local time = os.date("%X")
-		
-		local string = string.format("%-18s %s", time .. " " .. name, number)
-		table.insert(M.history, 1, string)
+		table.insert(M.index, 1, #M.index+1)
+		table.insert(M.history, 1, {time=time, name=name, number=number})
 	end
 end
 
